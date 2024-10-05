677a817b8f49cc7d4fb30f90a5e44f916490e468
Matt Motherway <mm@webxl.net>
2024-10-04 20:23:34 -0700

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 import theme from './theme.ts';
 
 import React from 'react';
 
 const root = document.getElementById('root');
+
 ReactDOM.createRoot(root!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <ColorModeProvider>
+        <App />
+      </ColorModeProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
