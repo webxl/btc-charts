e60c5f007f44032ab5383a026c7d2d7d1aa4c819
Matt Motherway <mm@webxl.net>
2025-10-02 10:43:55 -0700

    api fix

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ import { useColorMode } from '@chakra-ui/system';
 import { GitHub, Sliders } from 'react-feather';
 import { Header } from './sections/Header.tsx';
 import PowerLawChart from './charts/PowerLaw';
+import { fetchData } from './fetch';
 import { DailyPriceDatum } from './calc.ts';
 import { ChartSettings } from './charts/ChartControls.tsx';
 
@@ -53,49 +54,6 @@ const initialChartSettings: ChartSettings = {
   showInnerBand: false,
   showPricePlot: true,
   showHalvingEpochs: false
-};
-
-const apiUrl = (import.meta.env.VITE_API_URL as string) || '/api';
-
-const fetchData = async (callback: (data: DailyPriceDatum[]) => void) => {
-  try {
-    const data: DailyPriceDatum[] = await fetch(`${apiUrl}/timeseries`)
-      .then(r => {
-        if (!r.ok) {
-          throw new Error('Invalid network response');
-        }
-        return r.json() as Promise<DailyPriceDatum[]>;
-      })
-      .then(data => {
-        let _lastDate = dayjs(data[0].date);
-        const _dailyPriceData = [...data];
-        for (let i = 0; i < data.length; i++) {
-          const currentDate = dayjs(data[i].date);
-          const numDays = currentDate.diff(_lastDate, 'day');
-          if (numDays > 1) {
-            console.log(
-              'Missing data between',
-              _lastDate.format('YYYY-MM-DD'),
-              'and',
-              currentDate.format('YYYY-MM-DD')
-            );
-            // fill missing points
-            for (let j = 0; j < numDays; j++) {
-              _dailyPriceData.splice(i + j, 0, {
-                date: _lastDate.add(j + 1, 'day').format('YYYY-MM-DD'),
-                price: _dailyPriceData[i].price
-              });
-            }
-          }
-          _lastDate = currentDate;
-        }
-
-        return _dailyPriceData;
-      });
-    callback(data);
-  } catch (e) {
-    console.error(e);
-  }
 };
 
 function App() {
--- src/fetch.ts
+++ src/fetch.ts
@@ -1,5 +1,51 @@
+import { DailyPriceDatum } from "./calc";
+import dayjs from "dayjs";
+
+const apiUrl = (import.meta.env.VITE_API_URL as string) || '/api';
+
+export const fetchData = async (callback: (data: DailyPriceDatum[]) => void) => {
+  try {
+    const data: DailyPriceDatum[] = await fetch(`${apiUrl}/timeseries`)
+      .then(r => {
+        if (!r.ok) {
+          throw new Error('Invalid network response');
+        }
+        return r.json() as Promise<DailyPriceDatum[]>;
+      })
+      .then(data => {
+        let _lastDate = dayjs(data[0].date);
+        const _dailyPriceData = [...data];
+        for (let i = 0; i < data.length; i++) {
+          const currentDate = dayjs(data[i].date);
+          const numDays = currentDate.diff(_lastDate, 'day');
+          if (numDays > 1) {
+            console.log(
+              'Missing data between',
+              _lastDate.format('YYYY-MM-DD'),
+              'and',
+              currentDate.format('YYYY-MM-DD')
+            );
+            // fill missing points
+            for (let j = 0; j < numDays; j++) {
+              _dailyPriceData.splice(i + j, 0, {
+                date: _lastDate.add(j + 1, 'day').format('YYYY-MM-DD'),
+                price: _dailyPriceData[i].price
+              });
+            }
+          }
+          _lastDate = currentDate;
+        }
+
+        return _dailyPriceData;
+      });
+    callback(data);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
 export const fetchLatestPrice = async () => {
-  const response = await fetch('/api/latestprice');
+  const response = await fetch(`${apiUrl}/latestprice`);
   const data = await response.json();
   return data.price;
 };
